882aa18958b77a9434e94e502c5e157e76c74493
John Fields <jjfields24@outlook.com>
2020-04-07 00:34:30 -0400

    created UserSchema model

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
