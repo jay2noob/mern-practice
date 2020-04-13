29e8bee6b657b8eb4cebb315245f8ba461fca8b9
John Fields <jjfields24@outlook.com>
2020-04-13 00:11:54 -0400

    created Profile model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Posts route"));
+//const Post = require("../../models/Post");
+//const User = require("../../models/User");
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post("/", [ auth, [
+    check('text', 'Text is required').not().isEmpty()
+ ] ], 
+ async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return 
+  }
+};
 
 module.exports = router;
